ac91c6d23489abf3727fc6a98622bc7b47c54ef5
Vimlendu Kumar <23f2004267@ds.study.iitm.ac.in>
2026-03-13 22:51:24 +0530

    Milestone-PPA-V2 Auth-RBAC

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -3,26 +3,32 @@ from models import db
 from models.user import User
 from models.student import Student
 from models.company import Company
-from flask_jwt_extended import create_access_token
 
-auth_bp =Blueprint("auth", __name__)
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
+
+auth_bp = Blueprint("auth", __name__)
+
+
 
 @auth_bp.route("/register/student", methods=["POST"])
 def register_student():
-    data = request.get_json()
-    username=data.get("username")
-    password =data.get("password")
-    name=data.get("name")
-    email=data.get("email")
 
-    existing_user=User.query.filter_by(username=username).first()
+    data = request.get_json()
+
+    username = data.get("username")
+    password = data.get("password")
+    name = data.get("name")
+    email = data.get("email")
+
+    existing_user = User.query.filter_by(username=username).first()
 
     if existing_user:
-        return jsonify({"message":"Username already exists"}), 400
-    
+        return jsonify({"message": "Username already exists"}), 400
+
     new_user = User(
-        username = username,
-        password = password,
+        username=username,
+        password=generate_password_hash(password),
         role="student"
     )
 
@@ -30,44 +36,47 @@ def register_student():
     db.session.commit()
 
     student_profile = Student(
-        user_id = new_user.id,
+        user_id=new_user.id,
         name=name,
         email=email
     )
+
     db.session.add(student_profile)
     db.session.commit()
-    
+
     return jsonify({"message": "Student registered successfully"})
 
 
-
-@auth_bp.route("/register/company",methods=["POST"])
+@auth_bp.route("/register/company", methods=["POST"])
 def register_company():
-    
+
     data = request.get_json()
-    username=data.get("username")
-    password=data.get("password")
-    company_name=data.get("company_name")
-    website=data.get("website")
+
+    username = data.get("username")
+    password = data.get("password")
+    company_name = data.get("company_name")
+    website = data.get("website")
 
     existing_user = User.query.filter_by(username=username).first()
 
     if existing_user:
         return jsonify({"message": "Username already exists"}), 400
-    
+
     new_user = User(
         username=username,
-        password=password,
+        password=generate_password_hash(password),
         role="company"
     )
+
     db.session.add(new_user)
     db.session.commit()
 
     company_profile = Company(
-        user_id = new_user.id,
-        company_name = company_name,
+        user_id=new_user.id,
+        company_name=company_name,
         website=website
     )
+
     db.session.add(company_profile)
     db.session.commit()
 
@@ -77,27 +86,23 @@ def register_company():
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
+
     data = request.get_json()
 
-    username=data.get("username")
-    password=data.get("password")
+    username = data.get("username")
+    password = data.get("password")
 
-    user=User.query.filter_by(username=username).first()
-    
+    user = User.query.filter_by(username=username).first()
+
     if user is None:
         return jsonify({"message": "User not found"}), 404
-    
-    if user.password !=password:
+
+    if not check_password_hash(user.password, password):
         return jsonify({"message": "Incorrect password"}), 401
-    
+
     token = create_access_token(identity=user.id)
 
     return jsonify({
-        "token":token,
+        "token": token,
         "role": user.role
     })
-    
-
-
-
-
