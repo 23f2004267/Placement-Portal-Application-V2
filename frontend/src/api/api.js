a3ebbd67f94543c8f4825d204fb8e5edb9d05012
23f2004267 <230004267@ds.study.iitm.ac.in>
2026-05-01 01:23:32 +0530

    fix: set python version for psycopg2 compatibility

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:5000"
+    baseURL: "placement-portal-application-v2-production.up.railway.app"
 })
 
 API.interceptors.request.use(
