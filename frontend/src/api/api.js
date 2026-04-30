8373b1f9e2f718d6115b4d842285e04c236d7b6d
23f2004267 <230004267@ds.study.iitm.ac.in>
2026-05-01 03:01:22 +0530

    fix: frontend ready for deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "placement-portal-application-v2-production.up.railway.app"
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 API.interceptors.request.use(
